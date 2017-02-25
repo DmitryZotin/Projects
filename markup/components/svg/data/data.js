a44469368070a6cde1b14dd8d4917904e986cec2
Dima Zotin <zotindima@yandex.ru>
2017-02-25 11:29:19 +0500

    Что то сделал во второй раз

---
+++ markup/components/svg/data/data.js
@@ -0,0 +1,7 @@
+data = {
+    svgList: {
+        icon1: {icon: "{{Icon iconName='check-square-o' className='customClass' iconWidth='25' iconHeight='25'}}"},
+        icon2: {icon: "{{Icon iconName='check-square-o' className='customClass' iconWidth='25' iconHeight='25'}}"},
+    }
+
+}
